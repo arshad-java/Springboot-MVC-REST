0a7bf5ba7d8b773618c76559c78fabcb133f71c5
Arshadkhan93 <arshadkhan10062005@gmail.com>
2026-05-08 23:40:50 +0530

    Application.java

---
+++ Day01_MVC01_Wellcome1-1/src/main/java/com/example/demo/Day01Mvc01Wellcome11Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day01Mvc01Wellcome11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day01Mvc01Wellcome11Application.class, args);
+	}
+
+}
