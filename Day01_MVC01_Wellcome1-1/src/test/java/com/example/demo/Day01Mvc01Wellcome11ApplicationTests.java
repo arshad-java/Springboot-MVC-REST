baa4f9927d2be2c7c1486925de2f50f0b2199ad1
Arshadkhan93 <arshadkhan10062005@gmail.com>
2026-05-12 11:31:13 +0530

    Main class

---
+++ Day01_MVC01_Wellcome1-1/src/test/java/com/example/demo/Day01Mvc01Wellcome11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day01Mvc01Wellcome11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
