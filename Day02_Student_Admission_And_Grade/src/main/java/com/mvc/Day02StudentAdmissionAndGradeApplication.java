4787441d3780f74f56ad830f409f1a122605ab0d
Arshadkhan93 <arshadkhan10062005@gmail.com>
2026-05-13 12:29:06 +0530

    Main

---
+++ Day02_Student_Admission_And_Grade/src/main/java/com/mvc/Day02StudentAdmissionAndGradeApplication.java
@@ -0,0 +1,13 @@
+package com.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day02StudentAdmissionAndGradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day02StudentAdmissionAndGradeApplication.class, args);
+	}
+
+}
