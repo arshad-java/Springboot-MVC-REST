3b9d937f1c8c2ccb62da2682a4ba9f4ec5cc62a7
Arshadkhan93 <arshadkhan10062005@gmail.com>
2026-05-16 11:10:28 +0530

    entity class

---
+++ test-online-food-order/src/main/java/com/mvc/entity/TestFood.java
@@ -0,0 +1,61 @@
+package com.mvc.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TestFood {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	Long id;
+	@Column(length = 20)
+	String name;
+	@Column(length = 20)
+	String itemName;
+	Integer quantity;
+	Double price;
+	//Double totalAmount;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getItemName() {
+		return itemName;
+	}
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+	public Integer getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	
+	
+	
+
+}
