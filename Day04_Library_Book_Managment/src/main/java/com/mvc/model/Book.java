579be4664d2185f1569ecf37b7b1e9df3fd19ed1
Arshadkhan93 <arshadkhan10062005@gmail.com>
2026-05-20 21:51:25 +0530

    Entity Class Book

---
+++ Day04_Library_Book_Managment/src/main/java/com/mvc/model/Book.java
@@ -0,0 +1,69 @@
+package com.mvc.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	Integer id;
+	@Column(length = 15)
+	String title;
+	@Column(length = 15)
+	String author;
+	@Column(length = 15)
+	String genre;
+	Double price;
+	@Column(length = 15)
+	String availability;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getGenre() {
+		return genre;
+	}
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	public String getAvailability() {
+		return availability;
+	}
+	public void setAvailability(String availability) {
+		this.availability = availability;
+	}
+	
+	
+
+}
