8c9706a6b080d3b49aef4e959f0fdce27f65d5b2
Arshadkhan93 <arshadkhan10062005@gmail.com>
2026-05-20 21:51:40 +0530

    Main class

---
+++ Day04_Library_Book_Managment/src/main/java/com/mvc/Day04LibraryBookManagmentApplication.java
@@ -0,0 +1,13 @@
+package com.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day04LibraryBookManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day04LibraryBookManagmentApplication.class, args);
+	}
+
+}
