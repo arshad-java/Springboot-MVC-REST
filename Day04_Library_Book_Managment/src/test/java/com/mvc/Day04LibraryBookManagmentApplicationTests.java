f37a01543184143a4860cfd65a46544cfe4c2aac
Arshadkhan93 <arshadkhan10062005@gmail.com>
2026-06-03 22:01:12 +0530

    Main class

---
+++ Day04_Library_Book_Managment/src/test/java/com/mvc/Day04LibraryBookManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day04LibraryBookManagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
