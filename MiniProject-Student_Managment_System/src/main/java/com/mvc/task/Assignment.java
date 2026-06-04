a758730f8f5391bf43218bfebee8285c1d3143bd
Arshadkhan93 <arshadkhan10062005@gmail.com>
2026-06-04 10:09:02 +0530

    Assignment

---
+++ MiniProject-Student_Managment_System/src/main/java/com/mvc/task/Assignment.java
@@ -0,0 +1,79 @@
+package com.mvc.task;
+
+/*
+Mini Project: Student Management System
+========================================
+Objective
+Develop a Spring Boot MVC web application to perform basic CRUD operations on student records using Spring Data JPA.
+
+Classes Required:
+------------------
+Entity Class: Student
+//854868
+Attributes:
+
+id
+name
+email
+course
+
+Purpose:
+---------
+Represents student data stored in the database
+
+Repository Interface: StudentRepository
+
+Purpose:
+--------
+Perform CRUD operations using Spring Data JPA
+
+Service Class: StudentService
+
+Responsibilities:
+-----------------
+Save student
+Fetch all students
+Get student by ID
+Delete student
+
+Controller Class: StudentController
+
+Responsibilities:
+-----------------
+Show student list
+Display add student form
+Save student data
+Edit student data
+Delete student
+
+View Layer (JSP / Thymeleaf)
+----------------------------
+a) Add Student Page
+Form to enter student details
+
+b) Student List Page
+Display all students in table
+
+CRUD Functional Requirements
+
+Create
+Add a new student using form
+Save data into database
+
+Read
+Display all student records
+
+Update
+Edit existing student details
+Save updated data
+
+Delete
+Delete student record by ID
+
+Expected Outcome:
+-----------------
+Application should allow user to perform all four CRUD operations
+Data should be stored and retrieved from database
+Simple UI with form and table display 
+ */
+ 
