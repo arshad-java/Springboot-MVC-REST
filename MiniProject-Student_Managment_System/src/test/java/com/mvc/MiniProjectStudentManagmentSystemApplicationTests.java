4efd0556daa588c5ffb59b0d3b38ac0a1a0b1892
Arshadkhan93 <arshadkhan10062005@gmail.com>
2026-06-09 22:25:13 +0530

    mini project

---
+++ MiniProject-Student_Managment_System/src/test/java/com/mvc/MiniProjectStudentManagmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniProjectStudentManagmentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
