c4065b4d63919c3ae86e99a19a9851a601f91c1a
Arshadkhan93 <arshadkhan10062005@gmail.com>
2026-06-16 09:29:41 +0530

    Employee Salary

---
+++ Day02_Employee_Salary_Evoluation_System/src/main/java/com/mvc/Day02EmployeeSalaryEvoluationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day02EmployeeSalaryEvoluationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day02EmployeeSalaryEvoluationSystemApplication.class, args);
+	}
+
+}
