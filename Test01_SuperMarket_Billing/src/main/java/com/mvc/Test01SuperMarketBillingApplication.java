9a0e00075024789ad219a8d09685d079f60f904b
Arshadkhan93 <arshadkhan10062005@gmail.com>
2026-06-26 09:14:28 +0530

    test

---
+++ Test01_SuperMarket_Billing/src/main/java/com/mvc/Test01SuperMarketBillingApplication.java
@@ -0,0 +1,13 @@
+package com.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test01SuperMarketBillingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test01SuperMarketBillingApplication.class, args);
+	}
+
+}
