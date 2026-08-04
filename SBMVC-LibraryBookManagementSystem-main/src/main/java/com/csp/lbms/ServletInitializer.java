49d70f87324ed3c8052fa5ba1f3317cb121d94d9
Arshadkhan93 <arshadkhan10062005@gmail.com>
2026-08-04 23:26:09 +0530

    Servlet

---
+++ SBMVC-LibraryBookManagementSystem-main/src/main/java/com/csp/lbms/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.csp.lbms;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SbmvcLibraryBookManagementSystemApplication.class);
+	}
+
+}
