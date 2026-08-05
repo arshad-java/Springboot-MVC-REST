77833bfa8148ae94609a34ac00f16dc3c1129283
Arshadkhan93 <arshadkhan10062005@gmail.com>
2026-08-05 09:49:01 +0530

    RegisterDto.java

---
+++ SBMVC-LibraryBookManagementSystem-main/src/main/java/com/csp/lbms/dto/RegisterDto.java
@@ -0,0 +1,15 @@
+package com.csp.lbms.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterDto {
+    private String username;
+    private String password;
+    private String confirmPassword;
+    private String role;
+}
